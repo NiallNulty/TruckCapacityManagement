a0c568bc64f57d305ff315784954dda6051e93f7
Niall Nulty <niallnulty@gmail.com>
2025-02-04 12:52:04 +0000

    Initial Commit of Empty WinForm Application
    - Created application with appropriate name
    - Updated Assembly Info

---
+++ TruckCapacityManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TruckCapacityManagement")]
+[assembly: AssemblyDescription("Distributes Orders Fairly and Evenly Based on Truck Capacity")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RetailSolutions")]
+[assembly: AssemblyProduct("TruckCapacityManagement")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f508ea19-9077-42e5-8508-0c8e5ba210da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
